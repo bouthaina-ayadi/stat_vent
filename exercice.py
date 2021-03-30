f2306318a5aabf307d10de312de31a0887af1d80
ayedib <ayadib9@gmail.com>
2021-03-30 15:09:20 +0100

    name change

--- exercice.py
+++ exercice.py
@@ -4,9 +4,10 @@ import numpy as np
 
 tab_vent = np.loadtxt('wind.data', dtype=float)
 print(tab_vent)
-print(tab_vent.shape)
+print ( tab_vent.shape)
 print(tab_vent.size)
 stat = tab_vent[:,3: ]
+stat_size = stat.shape
 def dataset(t): #pour dataset
     print('vitesse minimal est:', t.min(), "vitesse max:", t.max(), "vitesse moyenne: ", t.mean(),"l'ecart type est: ", t.std() )
 
@@ -15,7 +16,7 @@ dataset(stat)
 
 def place(t, l1, l2, l3, l4): # pour chaque emplacement
     
-    for i in range(12):
+    for i in range(stat_size[1]):
        l1.append(t[: , i].min())
        l2.append(t[:, i].max())
        l3.append(t[:, i].mean())
@@ -28,7 +29,7 @@ print("le vent minimal pour chaque emplacement:" '\n', li1,'\n', "le vent max es
 
 def everyDay(t, c1, c2, c3, c4): # pour chaque jour
     
-    for j in range(3):
+    for j in range(stat_size[0]):
         c1.append(t[j, :].min())
         c2.append(t[j, :].max())
         c3.append(t[j, :].mean())
@@ -40,9 +41,13 @@ print("le vent minimal pour chaque jour:" '\n', ci1,'\n', "le vent max est: ", '
 
 
 #Question 5: l'endroit où la vitesse du vent est la plus élevée chaque jour.
+tab_size = tab_vent.shape
+def Q5(tab):
+    for k in range (tab_size[0]):
 
-print("levitesse le plus élevée le",)
+        print("le vitesse le plus élevée le",tab_vent[k:,2:], "/", tab_vent[k:,1:], "/", tab_vent[k:,0:], "est : ", tab_vent[k:,:].max())
 
+Q5(tab_vent)
 #question 6: l'année, le mois et le jour où la vitesse du vent la plus élevée a été enregistrée
 print("l'année, le mois et le jour où la vitesse du vent la plus élevée a été enregistrée est:", )
  
